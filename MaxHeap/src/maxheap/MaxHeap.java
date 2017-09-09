a28f80a05a3efb3f88998c35821046f281c08569
xeniazant <xeniazant@gmail.com>
2017-09-09 14:50:00 -0400

    please rename

--- MaxHeap/src/maxheap/MaxHeap.java
+++ MaxHeap/src/maxheap/MaxHeap.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication2;
+package maxheap;
 
 /**
  *
  * @author xeniazantello
  */
-public class JavaApplication2 {
+public class MaxHeap {
 
     /**
      * @param args the command line arguments
